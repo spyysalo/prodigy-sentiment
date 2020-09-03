4a24c1e49c15ce72b4a331edc9750908ac1a9246
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-09-03 16:04:35 +0300

    Update

--- start_all.sh
+++ start_all.sh
@@ -8,13 +8,14 @@ export PRODIGY_HOST=0.0.0.0
 
 for user in $users; do
     echo "Starting prodigy for $user on port $port"
+    echo "$user: http://ann.turkunlp.org:$port"
 
     export PRODIGY_PORT="$port"
     export PRODIGY_BASIC_AUTH_USER="$user"
     export PRODIGY_BASIC_AUTH_PASS=sentiment
     python -m prodigy sentiment sentiment-"$user" docsample.jsonl "$user" \
 	   -F sentiment.py &
-    sleep 10
+    sleep 3
     
     port=$((port+1))
 done
--- stop_all.sh
+++ stop_all.sh
@@ -4,5 +4,5 @@ ps aux | egrep '^'"$USER"'\b' | egrep '[p]rodigy' | awk '{ print $2 }' | \
 while read p; do
     echo "Killing $p ...."
     kill $p
-    sleep 10
+    sleep 3
 done
